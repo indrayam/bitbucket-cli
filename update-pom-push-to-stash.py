9ce5b329efe91037f2f09b6c8e4a91b9b9331c7b
Anand Sharma <anasharm@cisco.com>
2015-01-24 10:59:53 -0500

    Update pom.xml in all Stash repos

--- update-pom-push-to-stash.py
+++ update-pom-push-to-stash.py
@@ -12,18 +12,6 @@ def update_pom_push_to_stash(ulist):
     for user in ulist:
         count += 1
 
-        # Delete Repo Locally
-        cmd = 'rm -rf ' + reponame(user)
-        args = shlex.split(cmd)
-        p = Popen(args,
-                  stdout=PIPE, stderr=PIPE, cwd=folder_path('repo_parent', user))
-        output, err = p.communicate()
-        rc = p.returncode
-        print(output.decode('utf-8'), end='')
-        if rc > 0:
-            print(err)
-        print(count, ":", user, "[Delete Repo Locally]")
-
         # Clone the Repo
         cmd = 'git clone ssh://git@gitscm.cisco.com/ttt/' + reponame(user) + '.git'
         args = shlex.split(cmd)
@@ -38,16 +26,16 @@ def update_pom_push_to_stash(ulist):
 
         # Update the pom.xml file
         pomfilepath = '/Users/anand/workspace/_python_/python-projects/ttt-stash-cli/tmp/' + reponame(user) + '/pom.xml'
-        f = open(pomfilepath, 'r')
-        content = f.read()
-        f.close()
+        fread = open(pomfilepath, 'r')
+        content = fread.read()
+        fread.close()
 
         content = content.replace('it.gis.citeis.train-the-trainer.sdaastraining',
                                   'it.gis.citeis.train-the-trainer.sdaastraining.' + user, 1)
 
-        f = open(pomfilepath, 'w')
-        f.write(content)
-        f.close()
+        fwrite = open(pomfilepath, 'w')
+        fwrite.write(content)
+        fwrite.close()
 
         # Git Add
         cmd = 'git add .'
